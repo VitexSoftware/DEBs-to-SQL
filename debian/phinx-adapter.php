34bee9f46e70b85e7322147f1eb856df4b88cfe7
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-11-28 00:54:31 +0100

    dependencies added

--- debian/phinx-adapter.php
+++ debian/phinx-adapter.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Debian Repository SQL indexer - Phinx database adapter.
+ * Multi AbraFlexi Setup - Phinx database adapter.
  *
  * @author Vítězslav Dvořák <info@vitexsoftware.cz>
  * @copyright  2021 Vitex Software
@@ -10,26 +10,40 @@
 
 include_once '/usr/share/php/EaseCore/Atom.php';
 include_once '/usr/share/php/EaseCore/Shared.php';
-include_once '/usr/share/php/EaseCore/Functions.php';
 include_once '/usr/share/php/EaseCore/Molecule.php';
 include_once '/usr/share/php/EaseCore/Logger/Logging.php';
 include_once '/usr/share/php/EaseCore/Sand.php';
+include_once '/usr/share/php/EaseCore/Functions.php';
+include_once '/usr/share/php/EaseCore/Logger/Message.php';
+include_once '/usr/share/php/EaseCore/Logger/Loggingable.php';
+include_once '/usr/share/php/EaseCore/Logger/Loggingable.php';
+include_once '/usr/share/php/EaseCore/Logger/ToMemory.php';
 include_once '/usr/share/php/EaseCore/RecordKey.php';
 include_once '/usr/share/php/EaseCore/Brick.php';
+include_once '/usr/share/php/EaseCore/Person.php';
+include_once '/usr/share/php/EaseCore/Anonym.php';
+include_once '/usr/share/php/EaseCore/User.php';
+include_once '/usr/share/php/EaseCore/Logger/ToStd.php';
+include_once '/usr/share/php/EaseCore/Logger/ToSyslog.php';
+include_once '/usr/share/php/EaseCore/Logger/ToConsole.php';
+include_once '/usr/share/php/EaseCore/Logger/Regent.php';
+include_once '/usr/share/php/EaseCore/Logger/ToMemory.php';
+include_once '/usr/share/php/EaseCore/Exception.php';
 include_once '/usr/share/php/EaseFluentPDO/Orm.php';
 include_once '/usr/share/php/EaseFluentPDO/Engine.php';
 
-$shared = \Ease\Shared::instanced();
-$shared->loadConfig('/etc/debs2sql/.env', true);
 
-$prefix = "/usr/lib/debs2sql/db/";
+if (file_exists('/etc/multiflexi/multiflexi.env')) {
+    \Ease\Shared::instanced()->loadConfig('/etc/multiflexi/multiflexi.env', true);
+}
+
+$prefix = "/usr/lib/multiflexi/db/";
 
 $sqlOptions = [];
 
 if (strstr(\Ease\Functions::cfg('DB_CONNECTION'), 'sqlite')) {
- $sqlOptions["database"] = "/var/lib/dbconfig-common/sqlite3/debs2sql/".basename(\Ease\Functions::cfg("DB_DATABASE"));
+    $sqlOptions["database"] = "/var/lib/dbconfig-common/sqlite3/multiflexi/" . basename(\Ease\Functions::cfg("DB_DATABASE"));
 }
-
 $engine = new \Ease\SQL\Engine(null, $sqlOptions);
 $cfg = [
     'paths' => [
