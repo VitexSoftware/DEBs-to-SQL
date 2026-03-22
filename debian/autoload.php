e98c23270dd031d7f34f065fd996ab6cdfe23551
CyberVitexus <info@vitexsoftware.cz>
2026-03-23 00:33:34 +0100

    debian autoload fix

--- debian/autoload.php
+++ debian/autoload.php
@@ -7,7 +7,7 @@
 // Debian autoloader for debs2sql
 // Load dependency autoloaders
 require_once '/usr/share/php/Ease/autoload.php';
-require_once '/usr/share/php/Envms/FluentPDO/autoload.php';
+require_once '/usr/share/php/EaseFluentPDO/autoload.php';
 require_once '/usr/share/php/EaseTWB5/autoload.php';
 require_once '/usr/share/php/League/CommonMark/autoload.php';
 
